7cc754ab7eec8bc5570ad84417e08cea095c612d
Cuan <cliffordcuan@gmail.com>
2019-09-12 11:21:24 +0000

    updates solution for ex01

--- 01_dockerfiles/ex01/teamspeak-setup.sh
+++ 01_dockerfiles/ex01/teamspeak-setup.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 apt-get update -y && apt-get upgrade -y
 apt-get install -y wget bzip2
-adduser --disabled-login --gecos "" teamspeak
-chmod +rwx .
-su teamspeak
 wget http://dl.4players.de/ts/releases/3.8.0/teamspeak3-server_linux_amd64-3.8.0.tar.bz2
 tar xvf teamspeak3-server_linux_amd64-3.8.0.tar.bz2
 rm teamspeak3-server_linux_amd64-3.8.0.tar.bz2
 cd teamspeak3-server_linux_amd64
-touch .ts3server_license_accepted
-./ts3server_startscript.sh start
+./ts3server_minimal_runscript.sh start
